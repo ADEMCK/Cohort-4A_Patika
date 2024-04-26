bf67d39cf0e26bff2221d4e8ba9cd254c4c2d08a
Adem Onur Kolaylı <ademckonur@gmail.com>
2024-04-26 19:50:40 +0300

    v1

---
+++ src/Week1/airTempAct.java
@@ -0,0 +1,29 @@
+package Week1;
+
+import java.util.Scanner;
+
+public class airTempAct {
+    public static void main(String[] args) {
+        // Değişken
+        int heat;
+        // Input
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Sıcaklık Giriniz: ");
+        heat = scanner.nextInt();
+        // if-else
+        if (heat < 5) {
+            System.out.println("Kayak yapabilirsin");
+        } else if (heat >= 5 && heat <= 15) {
+            System.out.println("Sinemaya gidebilirsin.");
+            if (heat >= 15) {
+                System.out.println("Ayrıca pikniğe de gidebilirsiniz.");
+            }
+        } else if (heat == 25) {
+            System.out.println("Pikniğe gidebilirsiniz. Ayrıca yüzmeye de gidebilirsiniz.");
+        } else if (heat > 15 && heat < 25) {
+            System.out.println("Pikniğe gidebilirsiniz. ");
+        } else {
+            System.out.println("Yüzmeye gidebilirsiniz.");
+        }
+    }
+}
