93c380e5a42565e28d0e3bf765c293e4e659b633
Adem Onur Kolaylı <ademckonur@gmail.com>
2024-05-01 18:33:23 +0300

    v1

---
+++ src/Week1/mukemmelSayilar.java
@@ -0,0 +1,28 @@
+package Week1;
+
+import java.util.Scanner;
+
+public class mukemmelSayilar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //Kullanıcıdan değer alma
+        System.out.print("Bir sayı giriniz: ");
+        int n = scanner.nextInt();
+
+        // Sayının kendisine kadar olan tüm bölenlerini kontrol et.
+        int toplam = 0;
+        for (int i = 1; i < n; i++) {
+
+            // Eğer i, sayıya tam bölünüyorsa, i'yi toplam değişkenine ekle.
+            if (n % i == 0) {
+                toplam += i;
+            }
+        }
+        //Toplam girilen sayıya eşitse mükemmel bir sayıdır.
+        if (toplam == n) {
+            System.out.print(n + " Mükemmel bir sayıdır.");
+        } else {
+            System.out.print(n + " Mükemmel bir sayı değildir.");
+        }
+    }
+}
