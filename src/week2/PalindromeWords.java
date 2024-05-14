1b0f8c1ce01a7ba162c1186d19f1fa4315104b1a
Adem Onur Kolaylı <ademckonur@gmail.com>
2024-05-15 02:44:38 +0300

    Week 2 ödev

--- src/week2/PalindromeWords.java
+++ src/week2/PalindromeWords.java
@@ -2,27 +2,28 @@ package week2;
 
 public class PalindromeWords {
 
+    // Bu fonksiyon, verilen bir string'in palindrom olup olmadığını kontrol eder.
     static boolean isPalindrome(String str) {
+        int i = 0; // Kelimenin başlangıç indeksi
+        int j = str.length() - 1; // Kelimenin bitiş indeksi
 
-        int i = 0;                                      //Kelimenin ilk harfinin indeksini girdik.
-        int j = str.length()-1 ;                        //Kelimenin son harfinin indeksini girdik.
-
-        while (i < j) {                                 //Döngünün i'nin j'den küçük olduğu ana kadar dönmesini istiyoruz.
-            if (str.charAt(i) != str.charAt(j)) {       //i ve j indeksleri charAt ile harfleri gezerek eşit olmadığı sürece false dönecek.
-
+        // Başlangıç indeksi bitiş indeksinden küçük olduğu sürece döngüye gir.
+        while (i < j) {
+            // Başlangıç ve bitiş indekslerindeki karakterler eşleşmiyorsa,
+            // bu kelimenin palindrom olmadığını belirtir ve false döndür.
+            if (str.charAt(i) != str.charAt(j)) {
                 return false;
             }
-            i++;
-            j--;
+            i++; // Başlangıç indeksini artır
+            j--; // Bitiş indeksini azalt
         }
-        return true;                                    //if dışındaki durumlarda true dönecek.
+
+        // Yukarıdaki koşullar sağlanmıyorsa, kelime palindromdur ve true döndür.
+        return true;
     }
 
     public static void main(String[] args) {
-
+        // Test için "kayak" kelimesini kontrol et ve sonucu yazdır.
         System.out.println(isPalindrome("kayak"));
     }
-
 }
-
-
